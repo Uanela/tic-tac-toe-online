280433c898fc9cdd7eb26eb0dd354e4c91fb13c7
uanela <uanelaluiswayne@gmail.com>
2026-06-02 22:38:54 +0200

    chore(frontend-react): import with type

--- frontend-react/src/pages/auth/signup.page.tsx
+++ frontend-react/src/pages/auth/signup.page.tsx
@@ -1,4 +1,4 @@
-import { useState, FormEvent } from "react";
+import { useState, type FormEvent } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../utils/contexts/auth.context";
 import styles from "./Auth.module.css";
