73d391f79bc00b4e217408e65eb5149e6de7ff26
uanela <uanelaluiswayne@gmail.com>
2026-06-08 09:01:20 +0200

    feat: allow updating password

--- backend/src/modules/auth/schemas/update-me.schema.ts
+++ backend/src/modules/auth/schemas/update-me.schema.ts
@@ -1,7 +1,11 @@
 import { z } from "zod";
 
 const UpdateMeSchema = z.object({
-  email: z.string().trim().toLowerCase().optional(),
+  password: z
+    .string()
+    .min(8, "A palavra-passe deve ter no minímo 8 caractéres")
+    .regex(/[a-z]/, "A palavra-passe deve conter ao menos uma letra minúscula")
+    .regex(/[A-Z]/, "A palavra-passe deve conter ao menos uma letra maiúscula"),
 });
 
 export default UpdateMeSchema;
--- backend/src/modules/user/schemas/update-user.schema.ts
+++ backend/src/modules/user/schemas/update-user.schema.ts
@@ -1,15 +1,19 @@
-import { z } from "zod"
-import apiActions from "@/src/utils/validation/api-actions"
+import { z } from "zod";
+import apiActions from "@/src/utils/validation/api-actions";
 
 const UpdateUserSchema = z.object({
-  email: z.string()
-    .optional(),
+  email: z.string().optional(),
   isSuperUser: z.boolean().optional(),
   isStaff: z.boolean().optional(),
   isActive: z.boolean().optional(),
   role: z.string().optional(),
-})
+  password: z
+    .string()
+    .min(8, "A palavra-passe deve ter no minímo 8 caractéres")
+    .regex(/[a-z]/, "A palavra-passe deve conter ao menos uma letra minúscula")
+    .regex(/[A-Z]/, "A palavra-passe deve conter ao menos uma letra maiúscula"),
+});
 
-export default UpdateUserSchema
+export default UpdateUserSchema;
 
-export type UpdateUserSchemaType = z.infer<typeof UpdateUserSchema>
+export type UpdateUserSchemaType = z.infer<typeof UpdateUserSchema>;
